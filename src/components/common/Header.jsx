a4f2184761a3e0afb9d16ad3847327919317f090
atik074 <atiksmats@gmail.com>
2024-11-27 23:20:04 +0530

    jwt implement with axios done

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -3,8 +3,10 @@ import NotificationIcon from "../../assets/icons/notification.svg"
 import AvaterOne from "../../assets/images/avatars/avatar-circle.svg"
 import { Link } from "react-router-dom";
 import LogOut from "../auth/LogOut";
+import { useAuth } from "../../hooks/useAuth";
 
 const Header = () => {
+  const {auth} = useAuth()
     return (
         <nav className="sticky top-0 z-50 border-b border-[#3F3F3F] bg-[#1E1F24] py-4">
     <div className="container flex flex-col items-center justify-between gap-6 sm:flex-row">
@@ -24,11 +26,11 @@ const Header = () => {
           <img src={NotificationIcon}    alt="Notification" />
         </button>
          <LogOut/>
-        <button className="flex-center !ml-8 gap-3">
-          <span className="text-lg font-medium lg:text-xl">Atik</span>
+        <Link to='/me' className="flex-center !ml-8 gap-3">
+          <span className="text-lg font-medium lg:text-xl">{auth?.user?.firstName}</span>
           <img className="max-h-[32px] max-w-[32px] lg:max-h-[44px] lg:max-w-[44px]"
             src={AvaterOne} alt="" />
-        </button>
+        </Link>
       </div>
    
     </div>
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,9 +1,50 @@
+import { useEffect, useState } from "react";
+import { useAuth } from "../../hooks/useAuth";
+import { useAxios } from "../../hooks/useAxios";
+
 const Profile = () => {
-    return (
-        <div>
-            profile page comming ...
-        </div>
-    );
+  const [user, setUser] = useState(null);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const { auth } = useAuth();
+  const { api } = useAxios();
+
+ 
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        setLoading(true);
+        const response = await api.get(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/profile/${auth?.user?.id}`
+        );
+        setUser(response?.data?.user);
+        setPosts(response?.data?.posts);
+      } catch (error) {
+      
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  if (loading) {
+    return <div>Posts profile data is fetched...</div>;
+  }
+  if (error) {
+    return <div>Fetched posts Error...</div>;
+  }
+
+  return (
+    <div>
+      Wellcome , {user?.firstName}
+      <p>{posts.length}</p>
+    </div>
+  );
 };
 
 export default Profile;
