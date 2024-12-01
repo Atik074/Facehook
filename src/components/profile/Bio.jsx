d70e22e79904563c1a2dceaa5bf56113114ac852
atik074 <atiksmats@gmail.com>
2024-12-01 18:18:50 +0530

    login image and bio image change issue

--- src/components/profile/Bio.jsx
+++ src/components/profile/Bio.jsx
@@ -1,12 +1,36 @@
 import { useProfile } from "../../hooks/useProfile";
 import EditIcon from "../../assets/icons/edit.svg"
 import { useState } from "react";
-import CheakIcon from "../../assets/icons/close.svg"
+import CheakIcon from "../../assets/icons/arrow-circle.svg"
+import { useAxios } from "../../hooks/useAxios";
+import { actions } from "../../actions";
 
 const Bio = () => {
-    const {state} = useProfile()
+    const {state ,dispatch} = useProfile()
+    const {api} = useAxios()
     const [bio , setBio] = useState(state?.user?.bio)
     const [editMode , setEditMode] = useState()
+
+    const handleEditBio =async()=>{
+        dispatch({type:actions.profile.DATA_FETCHING})
+
+        try{
+           const response = await api.patch(`${import.meta.env.VITE_SERVER_BASE_URL}/profile/${state?.user?.id}`,{bio})
+
+           if(response.status === 200){
+                 dispatch({type:actions.profile.USER_DATA_EDITED , data:response.data})
+           }
+              setEditMode(false)
+
+
+        }catch(error){
+            console.log(error)
+            dispatch({type:actions.profile.DATA_FETCH_ERROR , error:error.message})
+
+        }
+
+
+    }
 
     return (
          <>
@@ -37,7 +61,9 @@ const Bio = () => {
                  alt="Edit" />
               </button>):
               ( 
-                 <button className="flex-center h-7 w-7 rounded-full">
+                 <button 
+                   onClick={handleEditBio}
+                 className="flex-center bg-white -mt-6 h-12 w-12 rounded-full">
           <img src={CheakIcon} alt="Edit" />
         </button>
         )
