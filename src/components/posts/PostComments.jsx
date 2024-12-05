9dad9aa28ba4e61fd3cde17ea9a597f635a0e7ef
atik074 <atiksmats@gmail.com>
2024-12-05 14:33:08 +0530

    you can comment on my project now

--- src/components/posts/PostComments.jsx
+++ src/components/posts/PostComments.jsx
@@ -1,15 +1,46 @@
 /* eslint-disable react/prop-types */
-import { useAvatar } from "../../hooks/useAvatar";
+import { useState } from "react";
 import PostCommentList from "./PostCommentList";
+import { useAxios } from "../../hooks/useAxios";
+import { useAuth } from "../../hooks/useAuth";
 
 const PostComments = ({post}) => {
-    const { avatarUrl } = useAvatar(post);
+  const {auth} = useAuth()
+    const [comments , setComments] = useState(post?.comments)
+    const [comment,setComment] = useState("")
+    const {api}  = useAxios()
+
+    const addComment = async (event) => {
+        const keyCode = event.keyCode;
+
+        if (keyCode === 13) {
+            try {
+                const response = await api.patch(
+                    `${import.meta.env.VITE_SERVER_BASE_URL}/posts/${
+                        post.id
+                    }/comment`,
+                    { comment }
+                );
+
+                if (response.status === 200) {
+                    setComments([...response.data.comments]);
+                    setComment('')
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    };
+
+
     return (
         <div>
         <div className="flex-center mb-3 gap-2 lg:gap-4">
             <img
                 className="max-w-7 max-h-7 rounded-full lg:max-h-[34px] lg:max-w-[34px]"
-                src={avatarUrl}
+                src={`${import.meta.env.VITE_SERVER_BASE_URL}/${
+                    auth?.user?.avatar
+                }`}
                 alt="avatar"
             />
 
@@ -18,6 +49,9 @@ const PostComments = ({post}) => {
                     type="text"
                     className="h-8 w-full rounded-full bg-lighterDark px-4 text-xs focus:outline-none sm:h-[38px]"
                     name="post"
+                    value={comment}
+                    onChange={(e)=>setComment(e.target.value)}
+                    onKeyDown={(e) => addComment(e)}
                     id="post"
                     placeholder="What's on your mind?"
                 />
@@ -29,7 +63,7 @@ const PostComments = ({post}) => {
             </button>
         </div>
 
-        <PostCommentList comments={post?.comments} />
+        <PostCommentList comments={comments} />
     </div>
     );
 };
